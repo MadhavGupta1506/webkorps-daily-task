542d5002a8cd92c12c7feb4019c17fa6f299272b
MadhavGupta1506 <gmadhav857@gmail.com>
2026-01-16 18:50:49 +0530

    extracted data from form

--- marksheet/scripts.js
+++ marksheet/scripts.js
@@ -1,9 +1,16 @@
-let studentName=$(".name").val()
+let studentName,rollNo,subject1,subject2,subject3,subject4,subject5;
 console.log(studentName)
-$(".submit-button").on("click",()=>{  
+$(".submit-button").on("click",(evt)=>{  
     // $("body").empty();
-})
+    evt.preventDefault();
 
-function hello(){
-    console.log("hello")
-}
+    studentName= $(".name").val();
+    rollNo=$(".rollno").val();
+    subject1= $(".subject1").val()
+    subject2= $(".subject2").val()
+    subject3= $(".subject3").val()
+    subject4= $(".subject4").val()
+    subject5= $(".subject5").val()
+        
+
+})
